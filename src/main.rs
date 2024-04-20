e6708640cc69b8cd4c0289a635a43f66fa7eac49
JunaYa <ayac3j@gmail.com>
2024-04-20 12:58:50 +0800

    chore: generic csv to json and yaml output

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,14 @@ use clap::Parser;
 fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
     match opts.cmd {
-        SubCommand::Csv(csv_opts) => process_csv(&csv_opts.input, &csv_opts.output)?,
+        SubCommand::Csv(csv_opts) => {
+            let outout = if let Some(output) = csv_opts.output {
+                output.clone()
+            } else {
+                format!("output.{}", csv_opts.format)
+            };
+            process_csv(&csv_opts.input, outout, csv_opts.format)?;
+        }
     }
     Ok(())
 }
